45b7be05a91efd3ec9f46733f6ea8c045bfa5716
KirishanthRajaraj <rajarajkirishanth@gmail.com>
2024-08-01 17:41:33 +0200

    .

--- BlitzTypes API/obj/Debug/net8.0/BlitzTypes API.AssemblyInfo.cs
+++ BlitzTypes API/obj/Debug/net8.0/BlitzTypes API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlitzTypes API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b516843964fc78f1068d9c7dad651a54dc301ff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+892c2ae0b2a195947416c8aa991ef7620ae64166")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlitzTypes API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlitzTypes API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
